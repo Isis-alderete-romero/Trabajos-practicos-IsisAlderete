1e31dc98b70f81ff8a9f8a58589205f71bb10f6b
Isis-alderete-romero <52178337@ivt.edu.ar>
2026-06-02 09:49:57 -0300

    h

---
+++ TPN5/app.js
@@ -0,0 +1,8 @@
+//Declarar variable
+let numero1
+//Asignar valor
+numero1 = 10
+//Declarar y asignar al mismo tiempo
+let numero2 = 30
+//Mostrar la información
+console.log(numero1 + numero1 + numero2)
