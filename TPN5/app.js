d5f3717089fa6ea5c1078baaec17d5a8401190c5
Isis-alderete-romero <52178337@ivt.edu.ar>
2026-06-04 10:33:23 -0300

    ygre

--- TPN5/app.js
+++ TPN5/app.js
@@ -1,8 +1,29 @@
 //Declarar variable
 let numero1
+let numero2
+let nombreAlumno 
+let apellidoAlumno 
+let edadAlumno
+let precio1
+let precio2
+let precio3
 //Asignar valor
 numero1 = 10
+numero2= 30
+nombreAlumno= "Pedro"
+apellidoAlumno =" Ramirez"
+edadAlumno =67
+precio1=300
+precio2=400
+precio3=500
 //Declarar y asignar al mismo tiempo
-let numero2 = 30
+suma = numero1 + numero2
+resta = numero2 - numero1
+multiplicación = numero1 * numero2
+división = numero2/ numero1
 //Mostrar la información
-console.log(numero1 + numero1 + numero2)
+console.log("el resultado de la multiplicacion es:" + multiplicación);
+console.log("el resultado de la division es:" + división);
+alert("el resultado de la suma es:" + suma);
+alert("el resultado de la resta es:" + resta);
+console.log("El alumno:"+ nombreAlumno + apellidoAlumno +" tiene "+ edadAlumno + " años");
