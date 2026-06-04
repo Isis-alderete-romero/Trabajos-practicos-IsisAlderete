abeb465a135990f5a42bf86df195001cb0fe4a23
Isis-alderete-romero <52178337@ivt.edu.ar>
2026-06-04 10:39:13 -0300

    holaaaaa

--- TPN5/app.js
+++ TPN5/app.js
@@ -7,6 +7,9 @@ let edadAlumno
 let precio1
 let precio2
 let precio3
+let colorFavorito
+let comidaFavorita
+let numeroFavorito
 //Asignar valor
 numero1 = 10
 numero2= 30
@@ -16,14 +19,20 @@ edadAlumno =67
 precio1=300
 precio2=400
 precio3=500
+colorFavorito=
+comidaFavorita=
+numeroFavorito=
 //Declarar y asignar al mismo tiempo
 suma = numero1 + numero2
 resta = numero2 - numero1
 multiplicación = numero1 * numero2
 división = numero2/ numero1
+total=precio1 + precio2 + precio3
 //Mostrar la información
 console.log("el resultado de la multiplicacion es:" + multiplicación);
 console.log("el resultado de la division es:" + división);
 alert("el resultado de la suma es:" + suma);
 alert("el resultado de la resta es:" + resta);
 console.log("El alumno:"+ nombreAlumno + apellidoAlumno +" tiene "+ edadAlumno + " años");
+console.log("el total a pagar es:" + total);
+
