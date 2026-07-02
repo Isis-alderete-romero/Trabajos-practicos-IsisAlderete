247c8fe88228b55ff942b4eafd89183359179e63
alumno <52178337@ivt.edu.ar>
2026-07-02 10:44:53 -0300

    xc

--- tp6/app.js
+++ tp6/app.js
@@ -2,3 +2,5 @@ let boton1 = document.querySelector ("boton1")
 let boton2 = document.querySelector ("boton2")
 let boton3 = document.querySelector ("boton3")
 let boton4 = document.querySelector ("boton4")
+ 
+boton1.onlinck
