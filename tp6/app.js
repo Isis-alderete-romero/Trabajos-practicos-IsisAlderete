d9b74caf19b58f20fce44e11fd68e1a4190d0fa7
alumno <52178337@ivt.edu.ar>
2026-07-02 10:43:46 -0300

    ,

---
+++ tp6/app.js
@@ -0,0 +1,4 @@
+let boton1 = document.querySelector ("boton1")
+let boton2 = document.querySelector ("boton2")
+let boton3 = document.querySelector ("boton3")
+let boton4 = document.querySelector ("boton4")
