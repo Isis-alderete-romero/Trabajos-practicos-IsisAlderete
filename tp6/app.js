a0f337aee3a13a46cb8ccc512739bbabc9cdbac9
alumno <52178337@ivt.edu.ar>
2026-07-27 08:04:00 -0300

    h2

--- tp6/app.js
+++ tp6/app.js
@@ -1,6 +1,22 @@
-let boton1 = document.querySelector ("boton1")
-let boton2 = document.querySelector ("boton2")
-let boton3 = document.querySelector ("boton3")
-let boton4 = document.querySelector ("boton4")
- 
-boton1.onlinck
+let btn1 = document.querySelector ("#boton1")
+let btn2 = document.querySelector ("#boton2")
+let btn3 = document.querySelector ("#boton3")
+let btn4 = document.querySelector ("#boton4")
+let text = document.querySelector ( "#parrafo1")
+  
+btn1.onlinck = function () {
+    //   Modificar propiedades
+    cambiar_texto.textContent= "Hola"
+   }
+   boton.onlinck = function () {
+    //   Modificar propiedades
+    cambiar_texto.textContent= "Hola"
+   }
+   btn3.onlinck = function () {
+    //   Modificar propiedades
+    btn3.style.color = 'red'
+   }
+boton.onlinck = function () {
+ //   Modificar propiedades
+ cambiar_texto.textContent= "Hola"
+}
