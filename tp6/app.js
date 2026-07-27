c6235db8e887ae3be154d10d9186c6a5306456e4
alumno <52178337@ivt.edu.ar>
2026-07-27 08:18:09 -0300

    rhgeyu

--- tp6/app.js
+++ tp6/app.js
@@ -4,19 +4,13 @@ let btn3 = document.querySelector ("#boton3")
 let btn4 = document.querySelector ("#boton4")
 let text = document.querySelector ( "#parrafo1")
   
-btn1.onlinck = function () {
-    //   Modificar propiedades
-    cambiar_texto.textContent= "Hola"
-   }
-   boton.onlinck = function () {
-    //   Modificar propiedades
-    cambiar_texto.textContent= "Hola"
-   }
-   btn3.onlinck = function () {
-    //   Modificar propiedades
-    btn3.style.color = 'red'
-   }
-boton.onlinck = function () {
- //   Modificar propiedades
- cambiar_texto.textContent= "Hola"
+btn1.onclinck = function(){
+text.textContent= "Hola"
 }
+btn2.onclinck = function(){
+    //   Modificar propiedades
+    text.style.backgroundColor = 'red'
+    }
+
+
+  
