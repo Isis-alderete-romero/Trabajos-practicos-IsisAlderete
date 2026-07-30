20f73f82ad576d0e9382091adb9898ff79e0c12c
Isis-alderete-romero <52178337@ivt.edu.ar>
2026-07-30 10:26:22 -0300

    the end

--- tp6/app.js
+++ tp6/app.js
@@ -1,16 +1,35 @@
-let btn1 = document.querySelector ("#boton1")
+let btn1 = document.querySelector ("#btn1")
 let btn2 = document.querySelector ("#boton2")
 let btn3 = document.querySelector ("#boton3")
 let btn4 = document.querySelector ("#boton4")
+let btn5 = document.querySelector ("#btn5")
+let btn6 = document.querySelector ("#btn6")
 let text = document.querySelector ( "#parrafo1")
+
   
-btn1.onclinck = function(){
+btn1.onclick = function(){
 text.textContent= "Hola"
 }
-btn2.onclinck = function(){
+btn2.onclick = function(){
     //   Modificar propiedades
     text.style.backgroundColor = 'red'
+ }
+ btn3.onclick = function(){
+    text.style.color = 'darkgreen'
+}
+btn4.onclick = function(){
+    text.style.fontFamily ="IMPACT"
+}
+btn5.onclick = function(){
+    text.style.fontSize ="90px"
     }
+btn6.onclick = function(){
+        text.style.fontFamily ="Arial"
+        text.style.color = "blue"
+        text.style.backgroundColor = "cornflowerblue"
+        text.style.fontSize ="15px"
+        text.textContent= "Adios"
 
+    }
 
   
