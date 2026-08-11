ab3da72ca91b643937c3c8f4e8642535f0639597
alumno <52178337@ivt.edu.ar>
2026-08-11 09:44:26 -0300

    tjtryj

--- Tpn7/app.js
+++ Tpn7/app.js
@@ -40,14 +40,30 @@ btn4.onclick = function () {
 // Ejercicio 5
 let text5= document.querySelector ("#ej5")
 let btn5 =document.querySelector ("#btn5")
-let edad= 6
+let edad2= 70
 
 btn5.onclick = function () {
-    if ((edad > 6) && (edad < 11)) {
-        text4.textContent = "Niño(6 a 11)"
-    }    else if ((edad > 12) && (edad < 18)) {
-        text4.textContent= "Adolescente (12 a 18 años"
-    }    else if{
-        text4.textContent= "Joven (19 a 26 años)"
+    if ((edad2 > 6) && (edad2 < 11)) {
+        text5.textContent = "Niño(6 a 11)"
+    }    else if ((edad2 > 12) && (edad2 < 18)) {
+        text5.textContent= "Adolescente (12 a 18 años"
+    }    else if((edad2 > 19) && (edad2 < 26)){
+        text5.textContent= "Joven (19 a 26 años)"
+    }else if((edad2 > 27) && (edad2 < 59)){
+        text5.textContent= "Adulto (27 a 59 años)"
+    } else{ 
+        text5.textContent= "Anciano (60 años o más)"
+    }
+}
+// Ejercicio 6
+let text6= document.querySelector ("#ej6")
+let btn6 =document.querySelector ("#btn6")
+let dia = sabado
+
+btn6.onclick = function () {
+    if ((dia = "lunes") || (dia = "martes")) {
+        text2.textContent = "Es un día laborable"
+    }    else {
+        text2.textContent= "Es fin de semana"
     }
 }
